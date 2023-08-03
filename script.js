18db80b6d94a4e58a5e66ca77bb3dbfd06ee09ae
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-08-03 15:01:51 +0800

    added html elements and reworked code

---
+++ script.js
@@ -0,0 +1,96 @@
+// starting scores
+let playerScore= 0;
+let computerScore= 0;
+
+//brings html elements into js
+const buttonRock= document.querySelector("#Rock");
+const buttonPaper= document.querySelector("#Paper");
+const buttonScissors= document.querySelector("#Scissors");
+const divResult= document.querySelector(".result");
+const divPlayScore= document.querySelector(".playScore");
+const divCompScore= document.querySelector(".compScore");
+
+//makes buttons run necessary functions to play
+buttonRock.addEventListener("click", () => {
+    const playerSelection= "Rock";
+    const computerSelection= getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    getScores(playerScore, computerScore);
+    getWinner(playerScore, computerScore);
+});
+buttonPaper.addEventListener("click", () => {
+    const playerSelection= "Paper";
+    const computerSelection= getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    getScores(playerScore, computerScore);
+    getWinner(playerScore, computerScore);
+});
+buttonScissors.addEventListener("click", () => {
+    const playerSelection= "Scissors";
+    const computerSelection= getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    getScores(playerScore, computerScore);
+    getWinner(playerScore, computerScore);
+});
+
+//gets random computer choice
+function getComputerChoice(){
+    let rng = Math.floor(Math.random()* 3);
+        if (rng == 0){
+            return rng = "Rock";  
+        } else if (rng == 1){
+            return rng = "Paper";
+        } else {
+            return rng = "Scissors";
+        }   
+} 
+
+//determines result of one round
+function playRound(playerSelection, computerSelection){
+    if (
+        (playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")){
+            ++playerScore;
+            const p= document.createElement("p");
+            p.innerText= `You Won! ${playerSelection} beats ${computerSelection}`;
+            divResult.appendChild(p);
+    } else if (
+        (playerSelection == "Rock" && computerSelection == "Paper") ||
+        (playerSelection == "Paper" && computerSelection == "Scissors") ||
+        (playerSelection == "Scissors" && computerSelection == "Rock")){
+            ++computerScore;
+            const p= document.createElement("p");
+            p.innerText= `You Lost! ${computerSelection} beats ${playerSelection}`;
+            divResult.appendChild(p);
+    } else if (playerSelection == computerSelection){
+            const p= document.createElement("p");
+            p.innerText= `It was a Tie! Both players picked ${playerSelection}`;
+            divResult.appendChild(p);
+    }
+    
+}
+
+//shows current scores
+function getScores(playerScore, computerScore){
+    divPlayScore.innerText= `Player Score = ${playerScore}`;
+    divCompScore.innerText= `Computer Score = ${computerScore}`;
+}
+
+//displays text when a player reaches 5 wins
+function getWinner(playerScore, computerScore){
+    if (playerScore == 5){
+        const h1 = document.createElement("h1");
+        h1.innerText= `You beat the computer ${playerScore} to ${computerScore}!`;
+        divResult.appendChild(h1);
+    } else if (computerScore == 5){
+        const h1 = document.createElement("h1");
+        h1.innerText= `The computer beat you ${computerScore} to ${playerScore}`;
+        divResult.appendChild(h1);
+//prevents previous messages from displaying even if score is != 5
+    } else {
+        const h1 = document.createElement("h1");
+        h1.innerText= "";
+        divResult.appendChild(h1);
+    }
+}
